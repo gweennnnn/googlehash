57f6cf2c733069496022c257965af9a07ccb2110
gweennnnn <me@gwendolyn.info>
2016-02-11 20:01:35 +0000

    update to the parse files

--- PrelimRound/src/prelimround/Main.java
+++ PrelimRound/src/prelimround/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Simulation sim = new Simulation();
+		
+		ProcessFile processFile = new ProcessFile("mother_of_all_warehouses.in");
+		processFile.getFile(sim);
+		
+		//System.out.println(sim.products);
 	}
 
 }
--- PrelimRound/src/prelimround/ProcessFile.java
+++ PrelimRound/src/prelimround/ProcessFile.java
@@ -1,6 +1,7 @@
 package prelimround;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProcessFile {
@@ -10,26 +11,59 @@ public class ProcessFile {
 		this.filename = filename;
 	}
 	
-	public void getFile(Map map, Drone[] drones, Product[] products, Warehouse[] warehouses, Order[] orders) {
+	public void getFile(Simulation sim) {
 		File file = new File(filename);
 		Scanner sc = null;
 		
 		try {
 			sc = new Scanner(file);
 			//make map
+			System.out.println("CREATE MAP");
 			int maprow = sc.nextInt();
 			int mapcol = sc.nextInt();
 			int deadline = sc.nextInt();
 			int maxload = sc.nextInt();
 			sc.nextLine();
-			map = new Map(maprow, mapcol);
+			sim.map = (new Map(maprow, mapcol));
 			
 			//products
+			System.out.println("CREATE PRODUCTS");
 			int noOfProducts = sc.nextInt();
 			sc.nextLine();
+			
 			for(int i = 0; i < noOfProducts; i++) {
 				int productWeight = sc.nextInt();
+				Product newProd = new Product(productWeight, i);
+				System.out.println(newProd);
+				sim.products.add(newProd);
+			}
+			sc.nextLine();
+
+			//warehouse
+			int noOfWarehouses = sc.nextInt();
+			sc.nextLine();
+			for(int i = 0; i < noOfWarehouses; i++) {
+				int wRow = sc.nextInt();
+				int wCol = sc.nextInt();
+				Coords wCoord = new Coords(wRow, wCol);
+				sc.nextLine();
 				
+				ArrayList<Inventory> stock = new ArrayList<Inventory>();
+				for(int x = 0; x < noOfProducts; x++) {
+					int noOfStock = sc.nextInt();
+					Inventory newInventory = new Inventory(sim.products.get(x), noOfStock);
+					stock.add(newInventory);
+				}
+				Warehouse newWarehouse = new Warehouse(wCoord, stock);
+			}
+			sc.nextLine();
+			
+			//order
+			int noOfOrder = sc.nextInt();
+			sc.nextLine();
+			for(int i = 0; i < noOfOrder; i++) {
+				int oRow = sc.nextInt();
+				int oCol = sc.nextInt();
 			}
 			
 		} catch (FileNotFoundException e) {
