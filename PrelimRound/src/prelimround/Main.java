19b3a1ffa25b15af3ec7012f356e0f49f212c1db
gweennnnn <me@gwendolyn.info>
2016-02-11 19:08:26 +0000

    MY BAD NEW SIm

--- PrelimRound/src/prelimround/Main.java
+++ PrelimRound/src/prelimround/Main.java
@@ -3,18 +3,7 @@ package prelimround;
 public class Main {
 
 	public static void main(String[] args) {
-
-		
-		Map map = new Map();
-		Product[] products = new Product[1];
-		Drone[] drones = new Drone[1];
-		Warehouse[] warehouses = new Warehouse[1];
-		Order[] orders = new Order[1];
-		
-		ProcessFile processfile = new ProcessFile("mother_of_all_warehouses.in");
-		
-		processfile.getFile(map, drones, products, warehouses, orders);
-		map.toString();
+		Simulation sim = new Simulation();
 	}
 
 }
--- PrelimRound/src/prelimround/Simulation.java
+++ PrelimRound/src/prelimround/Simulation.java
@@ -6,13 +6,9 @@ public class Simulation {
 	Drone[] drones = new Drone[1];
 	Warehouse[] warehouses = new Warehouse[1];
 	Order[] orders = new Order[1];
-	public Simulation(Map map, Product[] products, Drone[] drones, Warehouse[] warehouses, Order[] orders) {
-		super();
-		this.map = map;
-		this.products = products;
-		this.drones = drones;
-		this.warehouses = warehouses;
-		this.orders = orders;
+	
+	public Simulation() {
+		
 	}
 
 }
