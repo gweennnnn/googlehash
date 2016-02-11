93e5e76dd88e35fa053667bdb09da68e1bc41bc4
biego <gxb319@bham.ac.uk>
2016-02-11 18:54:35 +0000

    updated order

--- PrelimRound/src/prelimround/Order.java
+++ PrelimRound/src/prelimround/Order.java
@@ -1,9 +1,44 @@
 package prelimround;
 
+import java.util.ArrayList;
+
 public class Order {
 
 	public Coords location;
-	public ArrayList<Product> itemList;
-	public boolean finished;
+	public int id;
+	private ArrayList<Product> itemList;
+	private boolean finished;
+	
+	public Order(Coords location, int id){
+		this.location = location;
+		this.id = id;
+		
+		itemList = new ArrayList<Product>();
+		finished = false;
+	}
+	
+	public String toString(){
+		return (location.toString() + "\n" + itemList.toString());
+	}
+	
+	public void addItem(Product item){
+		itemList.add(item);
+	}
+	
+	public void removeItem(int i){
+		itemList.remove(i);
+		checkFinished();
+	}
+	
+	public boolean isFinished(){
+		return finished;
+	}
+	
+	private void checkFinished(){
+		if (itemList.isEmpty()) {
+			finished = true;
+		}
+	}
+	
 	
 }
