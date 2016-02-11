ba53533050a59c4740c31703474a6a73349884e7
gweennnnn <me@gwendolyn.info>
2016-02-11 17:22:51 +0000

    First commit for prelim round

---
+++ PrelimRound/src/prelimround/ProcessFile.java
@@ -0,0 +1,61 @@
+package prelimround;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ProcessFile {
+	String filename;
+	
+	public ProcessFile(String filename) {
+		this.filename = filename;
+	}
+	
+	public char[][] getFileAsCharArray() {
+		File file = new File(filename);
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		sc.nextLine();
+		char[][] input = new char[row][col];
+		
+		for(int i = 0; i < row; i++) {
+			String currStr = sc.nextLine();
+			input[i] = currStr.toCharArray();
+		}
+		
+		return input;
+	}
+	
+	public String[] getFile() {
+		File file = new File(filename);
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		sc.nextLine();
+		String[] input = new String[row];
+		
+		for(int i = 0; i < row; i++) {
+			input[i] = sc.nextLine().trim();
+		}
+		
+		return input;
+	}
+}
